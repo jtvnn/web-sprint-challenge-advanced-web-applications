d8f7f7f7cebd015fe1923cd039bd4662aed40480
Jason Vann <vannjt1@icloud.com>
2025-09-02 09:51:22 -0400

    updateArticle

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,30 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import { NavLink, Routes, Route, useNavigate } from 'react-router-dom'
-import Articles from './Articles'
-import LoginForm from './LoginForm'
-import Message from './Message'
-import ArticleForm from './ArticleForm'
-import Spinner from './Spinner'
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { NavLink, Routes, Route, useNavigate } from "react-router-dom";
+import Articles from "./Articles";
+import LoginForm from "./LoginForm";
+import Message from "./Message";
+import ArticleForm from "./ArticleForm";
+import Spinner from "./Spinner";
+import axios from "axios";
 
-const articlesUrl = 'http://localhost:9000/api/articles'
-const loginUrl = 'http://localhost:9000/api/login'
+const articlesUrl = "http://localhost:9000/api/articles";
+const loginUrl = "http://localhost:9000/api/login";
 
 export default function App() {
   // ✨ MVP can be achieved with these states
-  const [message, setMessage] = useState('')
-  const [articles, setArticles] = useState([])
-  const [currentArticleId, setCurrentArticleId] = useState()
-  const [spinnerOn, setSpinnerOn] = useState(false)
+  const [message, setMessage] = useState("");
+  const [articles, setArticles] = useState([]);
+  const [currentArticleId, setCurrentArticleId] = useState();
+  const [spinnerOn, setSpinnerOn] = useState(false);
 
   // ✨ Research `useNavigate` in React Router v.6
-  const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ 
+  const navigate = useNavigate();
+  const redirectToLogin = () => {
+    /* ✨ implement */
     navigate("/");
-  }
-  const redirectToArticles = () => { /* ✨ implement */
+  };
+  const redirectToArticles = () => {
+    /* ✨ implement */
     navigate("/articles");
-   }
+  };
 
   const logout = () => {
     // ✨ implement
@@ -34,7 +36,7 @@ export default function App() {
     // using the helper above.
     localStorage.removeItem("token");
     redirectToLogin();
-  }
+  };
 
   const login = async ({ username, password }) => {
     // ✨ implement
@@ -56,8 +58,7 @@ export default function App() {
     } finally {
       setSpinnerOn(false);
     }
-
-  }
+  };
 
   const getArticles = () => {
     // ✨ implement
@@ -71,77 +72,122 @@ export default function App() {
     setMessage("");
     setSpinnerOn(true);
 
-    useEffect(() => {
-      const token = localStorage.getItem("token")
-      if(!token) {
-        redirectToLogin();
-      } else {
-        const fetchArticles = async () => {
-          try {
-            const response = await axios.get(
-              articlesUrl,
-              { headers: { Authorization: token }}
-            )
-            setArticles(response.data);
-          } catch (error) {
-            if (error?.response?.status == 401) logout()
-          }
+    //useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      redirectToLogin();
+    } else {
+      const fetchArticles = async () => {
+        try {
+          const response = await axios.get(articlesUrl, {
+            headers: { Authorization: token },
+          });
+          setArticles(response.data);
+        } catch (error) {
+          if (error?.response?.status == 401) logout();
         }
-        fetchArticles()
-      }
-    }, [])
-  }
+      };
+      fetchArticles();
+    }
+    //  }, [])
+  };
 
-  const postArticle = article => {
+  const postArticle = (article) => {
     // ✨ implement
     // The flow is very similar to the `getArticles` function.
     // You'll know what to do! Use log statements or breakpoints
     // to inspect the response from the server.
-  }
+    const token = localStorage.getItem("token");
+    if (!token) {
+      redirectToLogin();
+    } else {
+      const postArticle = async () => {
+        try {
+          const response = await axios.post(articlesUrl, article, {
+            headers: { Authorization: token },
+          });
+          setArticles([...articles, response.data]);
+          setMessage(response.data.message);
+        } catch (error) {
+          if (error?.response?.status == 401) logout();
+        }
+      };
+      postArticle();
+    }
+  };
 
   const updateArticle = ({ article_id, article }) => {
     // ✨ implement
     // You got this!
+    const token = localStorage.getItem("token");
+    if (!token) {
+      redirectToLogin();
+    } else {
+      const putArticle = async () => {
+        try {
+          const response = await axios.put(
+            `${articlesUrl}/${article_id}`,
+            article,
+            { headers: { Authorization: token } }
+          );
+          setArticles([...articles, response.data]);
+          setMessage(response.data.message);
+        } catch (error) {
+          if (error?.response?.status == 401) logout();
+        }
+      };
+      putArticle();
+    }
   }
+    const deleteArticle = (article_id) => {
+      // ✨ implement
+    };
 
-  const deleteArticle = article_id => {
-    // ✨ implement
-  }
+    return (
+      // ✨ fix the JSX: `Spinner`, `Message`, `LoginForm`, `ArticleForm` and `Articles` expect props ❗
+      <>
+        <Spinner on={spinnerOn} />
+        <Message message={message} />
+        <button id="logout" onClick={logout}>
+          Logout from app
+        </button>
+        <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}>
+          {" "}
+          {/* <-- do not change this line */}
+          <h1>Advanced Web Applications</h1>
+          <nav>
+            <NavLink id="loginScreen" to="/">
+              Login
+            </NavLink>
+            <NavLink id="articlesScreen" to="/articles">
+              Articles
+            </NavLink>
+          </nav>
+          <Routes>
+            <Route path="/" element={<LoginForm login={login} />} />
+            <Route
+              path="articles"
+              element={
+                <>
+                  <ArticleForm
+                    postArticle={postArticle}
+                    updateArticle={updateArticle}
+                    setCurrentArticleId={setCurrentArticleId}
+                    articles={articles}
+                  />
+                  <Articles
+                    articles={articles}
+                    setCurrentArticleId={setCurrentArticleId}
+                    deleteArticle={deleteArticle}
+                    getArticles={getArticles}
+                  />
+                </>
+              }
+            />
+          </Routes>
+          <footer>Bloom Institute of Technology 2024</footer>
+        </div>
+      </>
+    );
+  };
 
-  return (
-    // ✨ fix the JSX: `Spinner`, `Message`, `LoginForm`, `ArticleForm` and `Articles` expect props ❗
-    <>
-      <Spinner on={spinnerOn}/>
-      <Message message={message}/>
-      <button id="logout" onClick={logout}>Logout from app</button>
-      <div id="wrapper" style={{ opacity: spinnerOn ? "0.25" : "1" }}> {/* <-- do not change this line */}
-        <h1>Advanced Web Applications</h1>
-        <nav>
-          <NavLink id="loginScreen" to="/">Login</NavLink>
-          <NavLink id="articlesScreen" to="/articles">Articles</NavLink>
-        </nav>
-        <Routes>
-          <Route path="/" element={<LoginForm login={login}/>} />
-          <Route path="articles" element={
-            <>
-              <ArticleForm
-                postArticle={postArticle}
-                updateArticle={updateArticle}
-                setCurrentArticleId={setCurrentArticleId}
-                articles={articles}
-                
-              />
-              <Articles
-                articles={articles}
-                setCurrentArticleId={setCurrentArticleId}
-                deleteArticle={deleteArticle}
-                getArticles={getArticles}
-              />
-            </>
-          } />
-        </Routes>
-        <footer>Bloom Institute of Technology 2024</footer>
-      </div>
-    </>
-  )
-}
